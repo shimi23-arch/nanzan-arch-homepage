8198aa31a09514cdc57d038645e3357fd58f87d6
shimi23-arch <t.shimi8888@gmail.com>
2025-08-17 06:20:32 +0900

    Update album.js

--- js/album.js
+++ js/album.js
@@ -1,16 +1,17 @@
+import PhotoSwipeLightbox from './photoswipe-lightbox.esm.js';
+
 const jsonUrl = "https://script.google.com/macros/s/AKfycbyGHVozroQs-kKpO7InuWJQ8PTqfjnsEJ8JC6AcgWLc3llFiCHDXzObL99dDFSLKmAsww/exec";
+
+const galleryEl = document.getElementById('gallery');
 
 fetch(jsonUrl)
   .then(res => res.json())
   .then(data => {
-    const gallery = document.getElementById('gallery');
-
     data.forEach(item => {
       const link = document.createElement('a');
       link.href = item.src;
-      link.setAttribute('data-pswp-width', 800);
-      link.setAttribute('data-pswp-height', 600);
-      link.className = "gallery-item";
+      link.setAttribute('data-pswp-width', item.width || 800);
+      link.setAttribute('data-pswp-height', item.height || 600);
 
       const img = document.creatElement('img');
       img.src = item.src;
@@ -18,20 +19,17 @@ fetch(jsonUrl)
       img.style.width = "150px";
 
       link.appendChild(img);
-      gallery.appendChild(link);
+      galleryEl.appendChild(link);
     });
 
-    const pswpElement = document.querySelectorAll('.pswp')[0];
-    gallery.addEventListener('click', function(event) {
-      event.preventDefault();
-      const items = data.map(i => ({
-        src: i.src,
-        w: 800,
-        h: 600
-      }));
-      const options = { index: Array.form(gallery.children).indexOf(eent.target.parentNode) };
-      const galleryInstance = new PhotoSwipe(pswpElement, PhotoSwipeUI_Default, items, options);
-      galleryInstance.init();
+    const lightbox = new PhotoSwipeLightbox({
+      gallery: '#gallery',
+      children: 'a',
+      pswpModule: () => import('./photoswipe.esm.js')
     });
-  });
+    lightbox.init();
+  })
+.catch(err => {
+  console.error("JSON取得失敗:", err);
+});
         
