626d3de83c0dc5a214c877d4ec89e2c15f80eb71
shimi23-arch <t.shimi8888@gmail.com>
2025-08-17 05:19:22 +0900

    Create album.js

---
+++ album.js
@@ -0,0 +1,37 @@
+const jsonUrl = "https://script.google.com/macros/s/AKfycbxPYobacfKL2o5KzVY9VtHXxU1dvvrsm9L1BzwbefKMyhp5knm4tqk9rTEa5Doxdk5GJA/exec";
+
+fetch(jsonUrl)
+  .then(res => res.json())
+  .then(data => {
+    const gallery = document.getElementById('gallery');
+
+    data.forEach(item => {
+      const link = document.createElement('a');
+      link.href = item.src;
+      link.setAttribute('data-pswp-width', 800);
+      link.setAttribute('data-pswp-height', 600);
+      link.className = "gallery-item";
+
+      const img = document.creatElement('img');
+      img.src = item.src;
+      img.alt = item.name;
+      img.style.width = "150px";
+
+      link.appendChild(img);
+      gallery.appendChild(link);
+    });
+
+    const pswpElement = document.querySelectorAll('.pswp')[0];
+    gallery.addEventListener('click', function(event) {
+      event.preventDefault();
+      const items = data.map(i => ({
+        src: i.src,
+        w: 800,
+        h: 600
+      }));
+      const options = { index: Array.form(gallery.children).indexOf(eent.target.parentNode) };
+      const galleryInstance = new PhotoSwipe(pswpElement, PhotoSwipeUI_Default, items, options);
+      galleryInstance.init();
+    });
+  });
+        
