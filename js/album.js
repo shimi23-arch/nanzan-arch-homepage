658a00d3f8c2441ee6effc828204318c8f004e62
shimi23-arch <t.shimi8888@gmail.com>
2025-08-17 19:19:02 +0900

    Update album.js

--- js/album.js
+++ js/album.js
@@ -1,47 +1,15 @@
-import PhotoSwipeLightbox from './photoswipe-lightbox.esm.js';
-const jsonUrl = "https://script.google.com/macros/s/AKfycbxnEqU0Tmg6q7OMzu2t2uAT8ea7myHPO1WU1B83lYFDHtRvHdoV5HoxJjiaTEITpnLVOg/exec";
+[
+  {
+    "event": "test",
+    "description": "みんなで楽しく運動会をしました",
+    "link": "https://photos.app.goo.gl/xxxxxx",
+    "thumbnail": "https://drive.google.com/uc?export=view&id=DRIVE_FILE_ID1"
+  },
+  {
+    "event": "文化祭",
+    "description": "文化祭では演劇と展示を行いました",
+    "link": "https://photos.app.goo.gl/yyyyyy",
+    "thumbnail": "https://drive.google.com/uc?export=view&id=DRIVE_FILE_ID2"
+  }
+]
 
-fetch(jsonUrl)
-  .then(res => res.json())
-  .then(data => {
-    const galleryEl = document.getElementById('gallery');
-
-    data.forEach(eventData => {
-      // 行事ごとにセクション作成
-      const section = document.createElement('div');
-      section.className = "event-section";
-
-      const title = document.createElement('h2');
-      title.textContent = eventData.event;
-      section.appendChild(title);
-
-      const eventGallery = document.createElement('div');
-      eventGallery.className = 'event-gallery';
-      section.appendChild(eventGallery);
-
-      eventData.photos.forEach(item => {
-        const link = document.createElement('a');
-        link.href = item.src;
-        link.dataset.pswpWidth = 800;
-        link.dataset.pswpHeight = 600;
-
-        const img = document.createElement('img');
-        img.src = item.src;
-        img.alt = item.name;
-
-        link.appendChild(img);
-        eventGallery.appendChild(link);
-      });
-
-      galleryEl.appendChild(section);
-    });
-
-    
-    const lightbox = new PhotoSwipeLightbox({
-      gallery: '.event-gallery',
-      children: 'a',
-      pswpModule: () => import('./photoswipe.esm.js')
-    });
-    lightbox.init();
-  })
-  .catch(err => console.error("JSON取得失敗:", err));
